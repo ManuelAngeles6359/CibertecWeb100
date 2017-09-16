66a57129429db4f4e58b24440350c6cd341a22c0
ManuelAngeles <angeles6_359@hotmail.com>
2017-09-16 14:03:45 -0500

    Clase Sabado 20170916

---
+++ Cibertec/Cibertec.Repositories/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cibertec.Repositories
+{
+    public interface IRepository<T> where T: class
+    {
+
+        
+
+
+        bool Delete(T entity);
+        bool Update(T entity);
+        int Insert(T entity);
+        IEnumerable<T> GetList();
+        T GetById(int Id);
+
+
+    }
+}

faf362274ac3c49024dec4b6b500974769b835ff
ManuelAngeles <angeles6_359@hotmail.com>
2017-09-30 14:06:43 -0500

    Web Api; metodos GetList y GetById del modelo customer

---
+++ Cibertec/Cibertec.WebApi/Controllers/CustomerController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Cibertec.UnitOfWork;
+using Cibertec.Models;
+
+namespace Cibertec.WebApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Customer")]
+    public class CustomerController : Controller
+    {
+
+        private readonly IUnitOfWork _unit;
+
+        public CustomerController(IUnitOfWork unit)
+        {
+            _unit = unit;
+        }
+
+        public IActionResult GetList()
+        {
+            return Ok(_unit.Customers.GetList());
+        }
+
+
+        public IActionResult GetById(int id)
+        {
+            return Ok(_unit.Customers.GetById(id));
+        }
+
+    }
+}

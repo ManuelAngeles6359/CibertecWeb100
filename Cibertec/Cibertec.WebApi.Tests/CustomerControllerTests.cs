18b69e81608cd01d88461ba4db74415026b71f88
ManuelAngeles <angeles6_359@hotmail.com>
2017-10-13 22:27:24 -0500

    Comprobar cobertura de codigo con opencover y ReportGenerator

--- Cibertec/Cibertec.WebApi.Tests/CustomerControllerTests.cs
+++ Cibertec/Cibertec.WebApi.Tests/CustomerControllerTests.cs
@@ -1,5 +1,7 @@
+using Cibertec.Mocked;
 using Cibertec.Models;
 using Cibertec.Repositories.Dapper.Northwind;
+using Cibertec.UnitOfWork;
 using Cibertec.WebApi.Controllers;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
@@ -13,13 +15,18 @@ namespace Cibertec.WebApi.Tests
     {
 
         private readonly CustomerController _customerController;
+        private readonly IUnitOfWork _unitMocked;
+
 
         public CustomerControllerTests()
         {
-            _customerController = new CustomerController(new NorthwindUnitOfWork(ConfigSettings.Northwind));
+            var unitMocked = new UnitOfWorkMocked();
+            _unitMocked = unitMocked.GetInstance();
+            _customerController = new CustomerController(_unitMocked);
+            
         }
 
-        [Fact]
+        [Fact(DisplayName = "[CustomerController] Get List")]
         public void Test_Get_All()
         {
 
@@ -33,5 +40,107 @@ namespace Cibertec.WebApi.Tests
             model.Count.Should().BeGreaterThan(0);
 
         }
+
+        [Fact (DisplayName ="[CustomerController] Insert")]
+        public void Insert_Customer_Test()
+        {
+
+
+            var customer = new Customer
+            {
+                Id = 101,
+                City = "Lima",
+                Country = "Peru",
+                FirstName = "Manuel",
+                LastName = "Angeles",
+                Phone = "941719681"
+            };
+
+            var result = _customerController.Post(customer) as OkObjectResult;
+
+            result.Should().NotBeNull();
+            result.Value.Should().NotBeNull();
+
+            var model = Convert.ToInt32(result.Value);
+            model.Should().Be(101);
+
+        }
+
+
+        //[Fact(DisplayName ="[CustomerController] Update")]
+        //public void Update_Customer_Test()
+        //{
+        //    var customer = new Customer
+        //    {
+        //        Id = 1,
+        //        City = "Lima",
+        //        Country = "Peru",
+        //        FirstName = "Manuel",
+        //        LastName = "Angeles",
+        //        Phone = "941719681"
+        //    };
+
+            
+        //    var result = _customerController.Put(customer) as OkObjectResult;
+
+
+        //    result.Should().NotBeNull();
+        //    result.Value.Should().NotBeNull();
+
+
+        //    var model = result.Value?.GetType().GetProperty("Message").GetValue(result.Value);
+
+        //    model.Should().Be("The customer is updated");
+
+        //    var currentCustomer = _unitMocked.Customers.GetById(1);
+        //    currentCustomer.Should().NotBeNull();
+        //    currentCustomer.Id.Should().Be(customer.Id);
+        //    currentCustomer.City.Should().Be(customer.City);
+        //    currentCustomer.Country.Should().Be(customer.Country);
+        //    currentCustomer.FirstName.Should().Be(customer.FirstName);
+        //    currentCustomer.LastName.Should().Be(customer.LastName);
+        //    currentCustomer.Phone.Should().Be(customer.Phone);
+
+
+        //}
+
+
+        [Fact(DisplayName ="[CustomerController] Delete")]
+        public void Delete_Customer_Test()
+        {
+
+            var customer = new Customer
+            {
+                Id = 1
+            };
+
+            var result = _customerController.Delete(customer) as OkObjectResult;
+
+            result.Should().NotBeNull();
+            result.Value.Should().NotBeNull();
+
+            var model = Convert.ToBoolean(result.Value);
+            model.Should().BeTrue();
+
+        }
+
+        [Fact(DisplayName ="[CustomerController] Get By Id")]
+        public void GetById_Csutomer_Test()
+        {
+
+            var result = _customerController.GetById(1) as OkObjectResult;
+
+            result.Should().NotBeNull();
+
+            result.Value.Should().NotBeNull();
+
+            var model = result.Value as Customer;
+            model.Should().NotBeNull();
+            model.Id.Should().BeGreaterThan(0);
+
+
+        }
+
+
     }
 }
